df7135904a462df6bc30c2981ba628d998d57b5e
technetbytes <saqibullah@gmail.com>
2023-06-05 09:39:23 +0500

    remove common error

--- Python/paramiko/test.py
+++ Python/paramiko/test.py
@@ -1,9 +1,9 @@
 import paramiko
-remoteFilePath = '/root/90/proxy/proxy.csv'
+remoteFilePath = '/root/90/data/data.csv'
 localFilePath = 'tempproxies.csv'
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('66.109.20.100', username='root', password='D6w07e5#o$Y3#Khko')
+ssh.connect('', username='root', password='')
 sftp = ssh.open_sftp()
 sftp.get(remoteFilePath,localFilePath)
 sftp.close()
