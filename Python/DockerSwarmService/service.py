1a8889636b02676a7ab06cb98d26a7f03eeb9ccb
technetbytes <saqibullah@gmail.com>
2024-01-22 21:20:14 +0500

    init commit

---
+++ Python/DockerSwarmService/service.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import socket
+app = Flask(__name__)
+ 
+@app.route('/')
+def servicehost():
+    host_name = socket.gethostname()
+    return 'Service is hosting by '+ host_name
+ 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
