e4dcbf65282f8f6d87c181e39ac0de660f8bc237
technetbytes <saqibullah@gmail.com>
2024-01-25 14:01:44 +0500

    Update docker-compose file

--- Python/DockerSwarmService/service.py
+++ Python/DockerSwarmService/service.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def servicehost():
     host_name = socket.gethostname()
-    return 'Service is hosting by '+ host_name
+    return 'Service is hosting by '+ host_name + ' v2'
  
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
