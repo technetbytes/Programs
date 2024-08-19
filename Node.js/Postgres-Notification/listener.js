9f16777262b32443a02da0a0f8e09e0395186c8c
technetbytes <saqibullah@gmail.com>
2024-08-19 20:25:44 +0500

    Postgres Listen/Notify

---
+++ Node.js/Postgres-Notification/listener.js
@@ -0,0 +1,23 @@
+// File: setup.js
+// Load all the environment variables
+require('dotenv').config();
+const { Client } = require('pg');
+const connectionString = process.env.DATABASE_URL;
+const client = new Client({ connectionString });
+
+async function listenToNotifications() {
+    try {
+      // Connect to Postgres
+      await client.connect();
+      // Listen to specific channel in Postgres
+      // Attach a listener to notifications received
+      client.on('notification', (msg) => {
+        console.log('Notification received', msg.payload);
+      });
+      await client.query('LISTEN channel_name');
+      console.log('Listening for notifications on channel_name');
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  listenToNotifications().catch(console.log);
